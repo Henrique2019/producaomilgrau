e32a0c6701c941e55b614378ecc74071c7942450
henrique2019 <henriqueronald2016@gmail.com>
2021-12-20 02:48:30 +0000

    prod

--- src/service/Prod.tsx
+++ src/service/Prod.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 // consumindo api  produçao medral. link https://medralapi.glitch.me/ 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYmY5NDc2MmFiNGFhMmIwODg5MGIyZSIsImlhdCI6MTYzOTk0NTM0NywiZXhwIjoxNjQwMDMxNzQ3fQ.-8OW_L572eeBNjUUgTbj1RvXeQZUsLYVWk7JXlbt_CU"
 
 const Prod =  axios.create({
   baseURL: "https://apimedral.glitch.me",
